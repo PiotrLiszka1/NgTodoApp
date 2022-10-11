dc58d74e5c1ab33da5d66b3b26349faa598a6ba6
piotr <piotrliszka111@gmail.com>
2022-10-11 13:04:26 +0200

    added task model and *ngFor in list items

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../core/model/task';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  tasks: Task[] = [
+    {
+      taskName: 'Kupić kawę.',
+      taskComplete: false,
+    },
+    {
+      taskName: 'Zadzwonić gdzieś tam.',
+      taskComplete: false,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
